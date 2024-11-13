a39cb49b797ede4cc7e47a0f3ed8f2bae16d360e
elyesmo <elybournane@gmail.com>
2024-11-13 14:06:27 +0100

    auth modif

--- ProjetJs/server/controllers/authController.js
+++ ProjetJs/server/controllers/authController.js
@@ -5,14 +5,10 @@ const LoginHistory = require('../models/LoginHistory');
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, password, confirmPassword } = req.body;
+    const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
       return res.status(400).json({ message: 'All fields are required' });
-    }
-
-    if (password !== confirmPassword) {
-      return res.status(400).json({ message: 'The typed passwords are not same' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
