d9608b0dd7ab22c3a82bbe8bd98f86ff49f9ab99
sakalil <pro1tein@gmail.com>
2024-11-13 11:38:18 +0100

    little front

---
+++ ProjetJs/client/js/inscription.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+      
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas.");
+      return false;
+    }
+  
+  
+    return true;
+  }
+  
