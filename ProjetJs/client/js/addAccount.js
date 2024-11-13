e35b7bd1fc1c0a69764f2ca60ed4842716a506b9
sakalil <pro1tein@gmail.com>
2024-11-13 15:12:49 +0100

    ajouter compte

---
+++ ProjetJs/client/js/addAccount.js
@@ -0,0 +1,30 @@
+// addAccount.js
+$(document).ready(function () {
+    $("#account-form").submit(function (event) {
+      event.preventDefault();
+  
+      const accountData = {
+        name: $("#name").val(),
+        type: $("#type").val(),
+        low_balance_threshold: $("#threshold").val() ? parseFloat($("#threshold").val()) : null,
+      };
+  
+      const token = localStorage.getItem("token");
+  
+      $.ajax({
+        url: "http://localhost:3000/api/accounts/add",
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+        contentType: "application/json",
+        data: JSON.stringify(accountData),
+        success: function (response) {
+          alert(response.message);
+          window.location.href = "index.html"; // Redirect back to the main page after creation
+        },
+        error: function (error) {
+          alert("Erreur lors de la création du compte: " + error.responseJSON.message);
+        }
+      });
+    });
+  });
+  
